2320c825f9624b68c3a063e515fd324357459624
Vijay Vuyyuru <vijay.vuyyuru@viam.com>
2024-05-03 17:21:41 -0400

    it finally works

--- build.sh
+++ build.sh
@@ -12,5 +12,6 @@ then
     brew install python3-venv
 fi
 mkdir dist
+python3 -m pip install --upgrade --force-reinstall adafruit-blinka Adafruit-PlatformDetect
 python3 -m venv .venv && . .venv/bin/activate && pip3 install -r requirements.txt && python3 -m PyInstaller --onefile --hidden-import="googleapiclient" --path="/home/weatherbox/led-animate-module/src" src/main.py
 tar -czvf dist/archive.tar.gz dist/main
