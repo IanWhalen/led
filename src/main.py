caad5d803c67c7b8c01c8a995042f4e879e5f7f4
Vijay Vuyyuru <vijay.vuyyuru@viam.com>
2024-09-20 16:32:36 -0400

    push new scripts

--- src/main.py
+++ src/main.py
@@ -10,8 +10,6 @@ from models import LedModel
 from viam.module.module import Module
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 from viam.components.generic import Generic
-
-
 async def main():
     """This function creates and starts a new module, after adding all desired resource models.
     Resource creators must be registered to the resource registry before the module adds the resource model.
